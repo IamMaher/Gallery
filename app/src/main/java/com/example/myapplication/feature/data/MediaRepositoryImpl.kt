417fac786fb9dd6b27d56965b9e9b710a0b14b21
ahmedmaher <ahmed.maher@sigma-emea.com>
2024-01-09 09:57:57 +0200

    feat(media): implement media repository

--- app/src/main/java/com/example/myapplication/feature/data/MediaRepositoryImpl.kt
+++ app/src/main/java/com/example/myapplication/feature/data/MediaRepositoryImpl.kt
@@ -2,10 +2,16 @@ package com.example.myapplication.feature.data
 
 import android.content.ContentResolver
 import android.content.ContentValues
+import android.net.Uri
 import android.provider.MediaStore
 import com.example.gallery.feature.domain.model.Album
+import com.example.gallery.feature.domain.model.Media
 import com.example.myapplication.core.Resource
+import com.example.myapplication.feature.data.media.findMedia
 import com.example.myapplication.feature.data.media.getAlbums
+import com.example.myapplication.feature.data.media.getMedia
+import com.example.myapplication.feature.data.media.getMediaByUri
+import com.example.myapplication.feature.data.media.getMediaListByUris
 import com.example.myapplication.feature.domain.MediaRepository
 import com.example.myapplication.feature.presentation.album.AllowedMedia
 import com.example.myapplication.feature.presentation.album.AllowedMedia.BOTH
@@ -15,9 +21,67 @@ import com.example.myapplication.feature.presentation.album.AllowedMedia.VIDEOS
 /** base use-case handle IO Thread */
 class MediaRepositoryImpl(private val contentResolver: ContentResolver) : MediaRepository {
 
+    override fun getMedia(): Resource<List<Media>> {
+        return try {
+            Resource.Success(data = contentResolver.getMedia())
+        } catch (e: Exception) {
+            Resource.Error(message = e.localizedMessage ?: "An error occurred")
+        }
+    }
+
+    override fun getMediaByType(allowedMedia: AllowedMedia): Resource<List<Media>> {
+        return try {
+            val contentQuery = when (allowedMedia) {
+                PHOTOS -> ContentQuery.PhotoQuery()
+                VIDEOS -> ContentQuery.VideoQuery()
+                BOTH -> ContentQuery.MediaQuery()
+            }
+            Resource.Success(data = contentResolver.getMedia(mediaQuery = contentQuery))
+        } catch (e: Exception) {
+            Resource.Error(message = e.localizedMessage ?: "An error occurred")
+        }
+    }
+
     override fun getAlbums(): Resource<List<Album>> {
         return try {
-            Resource.Success(data = contentResolver.getAlbums())
+            val data = contentResolver.getAlbums().toMutableList()
+            Resource.Success(data = data.toList())
+        } catch (e: Exception) {
+            Resource.Error(message = e.localizedMessage ?: "An error occurred")
+        }
+    }
+
+    override fun getMediaById(mediaId: Long): Media? {
+        val contentQuery = ContentQuery.MediaQuery()
+        return contentResolver.findMedia(contentQuery)
+    }
+
+    override fun getMediaByAlbumId(albumId: Long): Resource<List<Media>> {
+        return try {
+            val contentQuery = ContentQuery.MediaQuery().copy(
+                selection = MediaStore.MediaColumns.BUCKET_ID + "= ?",
+                selectionArgs = arrayOf(albumId.toString())
+            )
+
+            Resource.Success(data = contentResolver.getMedia(contentQuery))
+        } catch (e: Exception) {
+            Resource.Error(message = e.localizedMessage ?: "An error occurred")
+        }
+    }
+
+    override fun getMediaByAlbumIdWithType(albumId: Long, allowedMedia: AllowedMedia): Resource<List<Media>> {
+        return try {
+            val mimeType = when (allowedMedia) {
+                PHOTOS -> "image%"
+                VIDEOS -> "video%"
+                BOTH -> "%/%"
+            }
+            val contentQuery = ContentQuery.MediaQuery().copy(
+                selection = MediaStore.MediaColumns.BUCKET_ID + "= ? and " + MediaStore.MediaColumns.MIME_TYPE + " like ?",
+                selectionArgs = arrayOf(albumId.toString(), mimeType)
+            )
+
+            Resource.Success(data = contentResolver.getMedia(contentQuery))
         } catch (e: Exception) {
             Resource.Error(message = e.localizedMessage ?: "An error occurred")
         }
@@ -37,6 +101,45 @@ class MediaRepositoryImpl(private val contentResolver: ContentResolver) : MediaR
             )
 
             Resource.Success(data = contentResolver.getAlbums(contentQuery))
+        } catch (e: Exception) {
+            Resource.Error(message = e.localizedMessage ?: "An error occurred")
+        }
+    }
+
+
+    override fun getMediaByUri(uriAsString: String): Resource<List<Media>> {
+        return try {
+            val media = contentResolver.getMediaByUri(Uri.parse(uriAsString))
+            if (media == null) {
+                Resource.Error(message = "Media could not be opened")
+            } else {
+                val contentQuery = ContentQuery.MediaQuery().copy(
+                    selection = MediaStore.MediaColumns.BUCKET_ID + "= ?",
+                    selectionArgs = arrayOf(media.albumID.toString())
+                )
+                Resource.Success(data = contentResolver.getMedia(contentQuery).ifEmpty { listOf(media) })
+            }
+        } catch (e: Exception) {
+            Resource.Error(message = e.localizedMessage ?: "An error occurred")
+        }
+    }
+
+
+    override fun getMediaListByUris(listOfUris: List<Uri>, reviewMode: Boolean): Resource<List<Media>> {
+        return try {
+            var mediaList = contentResolver.getMediaListByUris(listOfUris)
+            if (reviewMode) {
+                val contentQuery = ContentQuery.MediaQuery().copy(
+                    selection = MediaStore.MediaColumns.BUCKET_ID + "= ?",
+                    selectionArgs = arrayOf(mediaList.first().albumID.toString())
+                )
+                mediaList = contentResolver.getMedia(contentQuery)
+            }
+            if (mediaList.isEmpty()) {
+                Resource.Error(message = "Media could not be opened")
+            } else {
+                Resource.Success(data = mediaList)
+            }
         } catch (e: Exception) {
             Resource.Error(message = e.localizedMessage ?: "An error occurred")
         }
--- app/src/main/java/com/example/myapplication/feature/domain/MediaRepository.kt
+++ app/src/main/java/com/example/myapplication/feature/domain/MediaRepository.kt
@@ -1,14 +1,28 @@
 package com.example.myapplication.feature.domain
 
+import android.net.Uri
 import com.example.gallery.feature.domain.model.Album
+import com.example.gallery.feature.domain.model.Media
 import com.example.myapplication.core.Resource
 import com.example.myapplication.feature.presentation.album.AllowedMedia
 
+
 interface MediaRepository {
+    fun getMedia(): Resource<List<Media>>
+
+    fun getMediaByType(allowedMedia: AllowedMedia): Resource<List<Media>>
 
     fun getAlbums(): Resource<List<Album>>
 
+    fun getMediaById(mediaId: Long): Media?
+
+    fun getMediaByAlbumId(albumId: Long): Resource<List<Media>>
+
+    fun getMediaByAlbumIdWithType(albumId: Long, allowedMedia: AllowedMedia): Resource<List<Media>>
 
     fun getAlbumsWithType(allowedMedia: AllowedMedia): Resource<List<Album>>
 
+    fun getMediaByUri(uriAsString: String): Resource<List<Media>>
+
+    fun getMediaListByUris(listOfUris: List<Uri>, reviewMode: Boolean): Resource<List<Media>>
 }
